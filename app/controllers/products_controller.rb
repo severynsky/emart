0e5183220831ebe309f26778b080806dd1577494
Ivan Zagorodniy <izagor@softserveinc.com>
2015-04-02 18:32:59 +0300

    added devise gem

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!
   before_action :find_product, only: [:show, :edit, :update, :destroy]
   before_action :find_categories
 
